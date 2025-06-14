078726d860ebc18fb8f1a72ef288786d7720ccd8
cirine-d <cirine.dumazet@gmail.com>
2025-06-14 18:54:20 +0100

    Played around with adding a 2d texture to the 3d fruit model. Works but there are issues with lighting and texture disappearing after collisions.

--- src/gameObjects/Ingredient.tsx
+++ src/gameObjects/Ingredient.tsx
@@ -1,12 +1,15 @@
 import { RigidBody, interactionGroups } from '@react-three/rapier';
 import { INGREDIENTS, ingredientsDictionary } from '../data/constants';
+import { useGLTF } from '@react-three/drei';
 
 interface Props {
   name: string;
   startPosition: THREE.Vector3;
 }
+useGLTF.preload('assets/ingredients/strawberry.glb');
 
 export const Ingredient: React.FC<Props> = (props: Props) => {
+  const gltf = useGLTF('assets/ingredients/strawberry.glb');
   return (
     <RigidBody
       name={props.name}
@@ -15,10 +18,10 @@ export const Ingredient: React.FC<Props> = (props: Props) => {
       linearDamping={2}
       restitution={2}
     >
-      <mesh name={props.name} position={[props.startPosition.x, props.startPosition.y, props.startPosition.z]}>
-        <icosahedronGeometry args={[0.4]} />
-        <meshBasicMaterial color={ingredientsDictionary[props.name].color} />
-      </mesh>
+      {/* <mesh name={props.name} position={[props.startPosition.x, props.startPosition.y, props.startPosition.z]}> */}
+      <primitive object={gltf.scene} position={[props.startPosition.x, props.startPosition.y, props.startPosition.z]} />
+      {/* <meshBasicMaterial color={ingredientsDictionary[props.name].color} /> */}
+      {/* </mesh> */}
     </RigidBody>
   );
 };
